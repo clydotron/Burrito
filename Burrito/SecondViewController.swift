7076ae49f5d1c4c542efe726f1e7645cd5e98a72
Brian Green <bgreen007@cox.net>
2019-09-06 16:12:41 -0700

    Initial Commit

---
+++ Burrito/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Burrito
+//
+//  Created by Brian Green on 9/6/19.
+//  Copyright © 2019 Brian Green. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
